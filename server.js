e3fa51a39598f70da6dbca00eba76250bcfe79a6
Nick Jones <nick@nicksays.co.uk>
2012-02-24 17:07:54 +0000

    First pass at player movement

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var express = require('express');
 var app = express.createServer()
 var io = require('socket.io').listen(app);
 
+io.set('log level', 1); // Reduce the log messages
+
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
@@ -20,13 +22,36 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-    io.sockets.emit('A new player has joined.'); 
+    socket.emit('A new player has joined.'); 
+    
+    var player = new Entity({x: 10, y: 10}, 'green', false);
+    entities.push(player);
+    
+    socket.on('direction-update', function (data) {
+        if (data == 'N') {
+            player.orientation.y = -1;
+        } else if (data == 'S') {
+            player.orientation.y = 1;
+        } else if (data == 'W') {
+            player.orientation.x = -1;
+        } else if (data == 'E') {
+            player.orientation.x = 1;
+        } else if (data == '') {
+            player.orientation.x = 0;
+            player.orientation.y = 0;
+        }
+    })
 });
 
-var Entity = function(pos, team) {
+var Entity = function(pos, team, npc) {
     this.id = id++;
     this.pos = pos;
     this.team = team;
+    this.npc = npc;
+    this.orientation = {
+        x: 0,
+        y: 0
+    };
 }
 
 function createBots() {
@@ -34,7 +59,7 @@ function createBots() {
 
     for (var i in TEAMS) {
         for (var j = 0; j < TEAM_COUNT; j++) {
-            bots.push(new Entity({x:Math.random() * 700, y:Math.random() * 500}, TEAMS[i]));
+            bots.push(new Entity({x:Math.random() * 700, y:Math.random() * 500}, TEAMS[i], true));
         }
     }
 
@@ -43,8 +68,13 @@ function createBots() {
 
 function moveEveryone() {
     for (var i in entities) {
-        entities[i].pos.x += Math.random();
-        entities[i].pos.y += Math.random();
+        if (entities[i].npc) {
+            entities[i].pos.x += Math.random();
+            entities[i].pos.y += Math.random();
+        } else {
+            entities[i].pos.x += Math.random() * 20 * entities[i].orientation.x;
+            entities[i].pos.y += Math.random() * 20 * entities[i].orientation.y;
+        }
     }
 }
 
@@ -52,7 +82,7 @@ function moveEveryone() {
 function gameLoop() {
     moveEveryone();
     io.sockets.emit('update', entities); 
-    setTimeout(gameLoop, 1000/30); 
+    setTimeout(gameLoop, 1000/5); 
 }
 
 
